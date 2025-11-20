8e792b1b22c02712c004426095f26490487758ed
Rashmi Das <rashmidas333@outlook.com>
2025-11-20 22:39:04 +0100

    Assignment: Intro to JavaScript Week 3

---
+++ courses/foundation/intro-to-javascript/week3/exercise2.js
@@ -0,0 +1,21 @@
+// When will we be there??
+
+function calculateTravelTime(x, y) {
+  return x / y;
+}
+
+console.log(calculateTravelTime(50, 100));  
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+function getTravelTime(info) {
+  const time = info.destinationDistance / info.speed;
+   const hours = Math.floor(time);
+  const minutes = Math.round((time - hours) * 60);
+   return `${hours} hours and ${minutes} minutes`;
+}
+
+const finalTravelTime = getTravelTime(travelInformation);
+console.log(finalTravelTime);  
