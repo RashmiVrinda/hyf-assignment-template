48a99803e06221a40910d05964adfeb82167600c
Rashmi Das <rashmidas333@outlook.com>
2026-03-12 07:58:58 +0100

    exercise 1

---
+++ courses/backend/advanced-javascript/exercise1.js
@@ -0,0 +1,19 @@
+//Rewrite with array methods
+
+// Example structure for exercise1.js
+import { teas } from "./teas.js";
+
+
+// const result = [];
+// for (let i = 0; i < teas.length; i++) {
+//   if (teas[i].caffeineLevel !== "none") {
+//     result.push(teas[i].name.toUpperCase());
+//   }
+// }
+// console.log(result);
+
+const result = teas
+.filter(tea => tea.caffeineLevel !=="none")
+ .map(tea => tea.name.toUpperCase());
+
+console.log (result)
